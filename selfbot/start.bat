86b1664dc94c6ef15fc9d698fb7c761e84f84d2e
Sleek <42476966+s4dic@users.noreply.github.com>
2025-02-15 15:19:43 +0100

    Delete selfbot directory

--- selfbot/start.bat
+++
@@ -1,6 +0,0 @@
-npm i
-echo "Connexion au compte en cours..."
-while true; do
-   node index.js;
-   sleep 300;
-done
--- selfbot/start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-npm i
-echo "Connexion au compte en cours..."
-while true; do
-   node index.js;
-   sleep 300;
-done
