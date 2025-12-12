0d6eaa57957da3a5af35287387cd53c6b33b2f1e
Sleek <42476966+s4dic@users.noreply.github.com>
2025-12-12 18:50:34 +0100

    Create config.ts

---
+++ BetterDiscord Plugins/encryption/master/src/lib/config.ts
@@ -0,0 +1,24 @@
+export const config = {
+	name: "encryptionPlugin",
+	nameTitle: "Encryption",
+	description: "Experimental message encryption using AES-GCM",
+	author: {
+		name: "Harry Merritt & Sleek",
+		github_username: "s4dic",
+	},
+	version: {
+		current: "2.1.1",
+		latest: "",
+		update: false,
+		ignoreUpdate: false
+	},
+	link: {
+		repository: "https://github.com/s4dic/discord/new/main/BetterDiscord%20Plugins/encryption",
+		source: "https://raw.githubusercontent.com/s4dic/discord/refs/heads/main/BetterDiscord%20Plugins/encryption/encryption.plugin.js",
+		sourceConfig:
+			"https://github.com/s4dic/discord/tree/main/BetterDiscord%20Plugins/encryption/master/src/lib/config.ts"
+	}
+};
+
+export type Config = typeof config;
+export type UserData = Record<string, { password: string; state: boolean }>;
